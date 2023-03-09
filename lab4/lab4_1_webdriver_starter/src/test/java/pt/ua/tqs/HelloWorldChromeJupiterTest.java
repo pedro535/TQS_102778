b464cc8d0bf64e553372cc9eb4c701e6f70c80cb
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-03-09 16:00:49 +0000

    Add lab4_1 and lab4_2

---
+++ lab4/lab4_1_webdriver_starter/src/test/java/pt/ua/tqs/HelloWorldChromeJupiterTest.java
@@ -0,0 +1,40 @@
+package pt.ua.tqs;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloWorldChromeJupiterTest {
+    
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+    
+}
