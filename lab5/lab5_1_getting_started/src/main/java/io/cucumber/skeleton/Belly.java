5ffd3101d9298f7a024491a1a8aaf02bc0baed4f
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-03-16 15:03:07 +0000

    Add lab5_1 getting started

---
+++ lab5/lab5_1_getting_started/src/main/java/io/cucumber/skeleton/Belly.java
@@ -0,0 +1,15 @@
+package io.cucumber.skeleton;
+
+public class Belly {
+    public void eat(int cukes) {
+        System.out.println("Eating " + cukes + "cucke!");
+    }
+
+    public void wait(int hours) {
+        System.out.println("Waiting " + hours + "hours!");
+    }
+
+    public void growl() {
+        System.out.println("Growl!");
+    }
+}
---
+++ lab5/lab5_1_getting_started/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -0,0 +1,29 @@
+package io.cucumber.skeleton;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+    private Belly belly;
+
+    @Given("I have {int} cukes in my belly")
+    public void I_have_cukes_in_my_belly(int cukes) {
+        belly = new Belly();
+        belly.eat(cukes);
+    }
+
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) {
+        belly.wait(int1);
+    }
+    
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        belly.growl();
+    }
+
+}
