8aac9477bb71c35c9c909febef55146647de57c1
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-03-29 19:42:02 +0100

    Setup SpringBoot project for HW1

---
+++ HW1/airwatch/src/test/java/pt/ua/tqs/airwatch/AirwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.tqs.airwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
