97fb9be510f139e26a6e5e1eb4d6b8f508413929
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-03-30 15:12:18 +0100

    Add lab7_1

---
+++ lab7/lab7_1_rest_assured/src/test/java/pt/ua/tqs/PublicRestServicesTest.java
@@ -0,0 +1,55 @@
+package pt.ua.tqs;
+
+
+import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PublicRestServicesTest {
+
+    private String URL = "https://jsonplaceholder.typicode.com/todos";
+
+    @Test
+    @DisplayName("Test if the endpoint to list all ToDos is available (status code 200)")
+    void testAvailability() {
+        given().get(URL)
+            .then().assertThat().statusCode(200);
+    }
+
+
+    @Test
+    @DisplayName("Test if when querying for ToDo #4, the API returns an object with title “et porro tempora")
+    void testQueryingTodo4() {
+        int todoNum = 4;
+        given().get(URL + "/" + todoNum)
+            .then().assertThat().statusCode(200)
+            .and().body("id", equalTo(4))
+            .and().body("title", equalTo("et porro tempora"));
+    }
+
+
+    @Test
+    @DisplayName("Test if when listing all “todos”, you the results in less then 2secs")
+    void testIfResultInLessThan2Secs() {
+        given().get(URL)
+            .then().assertThat().statusCode(200)
+            .and().body("id", hasItems(198, 199));
+    }
+
+
+    @Test
+    @DisplayName("Test if when listing all “todos”, you the results in less then 2secs")
+    void testIf198And199Exist() {
+        long seconds = 2;
+
+        given().get(URL)
+            .then().assertThat().statusCode(200)
+            .and().time(lessThan(seconds), TimeUnit.SECONDS);
+    }
+
+}
